e70eb3cd70abef421b017ff78705f2207e3066c9
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-06 20:45:38 +0330

    sqlite usage added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { Reports } from './reports/reports.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: "sqlite",
-    database: "db.sqlite",
-    entities: [],
-    synchronize: true
-  }),UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Reports],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
