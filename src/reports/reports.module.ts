662f62afa5654f5e6275abe86899bfe84002cea4
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-06 20:44:12 +0330

    reports module updated to spport sqlite

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reports } from './reports.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reports])],
   providers: [ReportsService],
   controllers: [ReportsController]
 })
