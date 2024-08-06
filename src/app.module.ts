bd3708f2e996d54094f498949bbfccff10310626
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-06 20:12:56 +0330

    database added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: [],
+    synchronize: true
+  }),UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
