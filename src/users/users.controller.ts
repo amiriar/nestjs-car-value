e5c51f8da2d17e786c3b0d8f2f3c9b7728161665
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-07 09:58:46 +0330

    create user method added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService){}
+
+    @Post("/signup")
+    async createUser(@Body() body: createUserDto){
+        return await this.usersService.create(body.email, body.password)
+    }
+}
