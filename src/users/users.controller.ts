dbc4c2fb549e41d3d7d07468e0148aa1bf2dbc2f
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-07 18:13:56 +0330

    user controller updated

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,4 +11,28 @@ export class UsersController {
     async createUser(@Body() body: createUserDto){
         return await this.usersService.create(body.email, body.password)
     }
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Get("/:id")
+    async findUser(@Param('id') id: string){
+        const user = await this.usersService.findOne(parseInt(id))
+        if(!user) throw new NotFoundException()
+        return user
+    }
+
+    @Get()
+    async findAllUsers(@Query('email') email: string){
+        return await this.usersService.find(email)
+    }
+
+    @Patch('/:id')
+    async updateUser(@Param('id') id: string, @Body() body: updateUserDto){
+        return await this.usersService.update(parseInt(id), body)
+    }
+
+    @Delete('/:id')
+    async removeUser(@Param('id') id: string){
+        return await this.usersService.remove(parseInt(id))
+    }
+
 }
