e832c09327fa05ee2ddf5e91ad82144a48b9d633
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-10 09:04:53 +0330

    added changes to database

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
 import { AuthService } from './auth.serivce';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CurrentUserInterceptor } from './interceptors/currentUSer.interceptor';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
+// import { CurrentUserInterceptor } from './interceptors/currentUSer.interceptor';
+import { CurrentUSerMiddleware } from './middleware/current-user.mmiddleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -13,11 +14,15 @@ import { CurrentUserInterceptor } from './interceptors/currentUSer.interceptor';
   providers: [
     UsersService,
     AuthService,
-    // globaly scoped interceptor:
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor 
-    },
+    // // globaly scoped interceptor:
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CurrentUserInterceptor 
+    // },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(CurrentUSerMiddleware).forRoutes('*')
+  }
+}
