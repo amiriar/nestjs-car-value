7a8865d3ea92f634f7b33fd98d1f2441eba2caff
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-10 08:58:33 +0330

    updated report entity and added relations to user field

--- src/reports/reports.entity.ts
+++ src/reports/reports.entity.ts
@@ -1,10 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/users/users.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Reports{
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Column({ default: false })
+    approved: boolean;
+
     @Column()
     price: string;
+
+    @Column()
+    make: string;
+
+    @Column()
+    model: string;
+
+    @ManyToOne(() => User, (user) => user.reports)
+    user: User;
+
+    @Column()
+    year: number;
+
+    @Column()
+    lng: number;
+
+    @Column()
+    lat: number;
+
+    @Column()
+    mileage: number;
 }
