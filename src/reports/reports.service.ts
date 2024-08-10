3163c48f121361e4553b1df8b33afb7ebd133327
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-10 08:59:01 +0330

    updated report service to add the queries

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reports } from './reports.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/users.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Reports) private repo: Repository<Reports>) {}
+
+  async createEstimate({
+    make,
+    model,
+    lng,
+    lat,
+    mileage,
+    year,
+    approved
+  }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE', { approved })
+      .orderBy('ABS(mileage - :mileage)', "DESC")
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
+
+  async create(reportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(reportDto);
+    report.user = user;
+    return await this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+    report.approved = approved;
+    return await this.repo.save(report);
+  }
+}
