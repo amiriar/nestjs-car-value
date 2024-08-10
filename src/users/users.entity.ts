6c3192083aa29760ccc5d9496afadbe86ab30ce3
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-10 09:03:53 +0330

    updated user entitiy to support the relations and admin role

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,5 +1,14 @@
 import { Exclude } from 'class-transformer';
-import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Reports } from 'src/reports/reports.entity';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -9,22 +18,28 @@ export class User {
   @Column()
   email: string;
 
+  @Column({ default: true })
+  admin: boolean;
+
   @Column()
   @Exclude()
   password: string;
-  
+
+  @OneToMany(() => Reports, (report) => report.user)
+  reports: Reports[];
+
   @AfterInsert()
-  logInsert(){
+  logInsert() {
     console.log(`inserted user by the id of ${this.id}`);
   }
 
   @AfterUpdate()
-  logUpdate(){
+  logUpdate() {
     console.log(`updated user by the id of ${this.id}`);
   }
 
   @AfterRemove()
-  logRemove(){
+  logRemove() {
     console.log(`removed user by the id of ${this.id}`);
   }
 }
