febb101730b3947dc94d86cf5e09b784a3a7fb74
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-10 08:50:42 +0330

    added a not authorized error handler to auth guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,10 +1,13 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { Observable } from "rxjs";
 
 export class AuthGuard implements CanActivate{
     constructor(){}
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest()
+        
+        if(!request.session.userId)
+            throw new UnauthorizedException()
         return request.session.userId
     }
 }
